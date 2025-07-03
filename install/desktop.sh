eda74631bed5369d549bd62d66e099168b21471b
vitor <vitordonnangeloc@proton.me>
2025-07-03 12:14:25 -0300

    Remove 1password and chromium

--- install/desktop.sh
+++ install/desktop.sh
@@ -1,8 +1,6 @@
 yay -S --noconfirm --needed \
   brightnessctl playerctl pamixer pavucontrol wireplumber \
   fcitx5 fcitx5-gtk fcitx5-qt fcitx5-configtool \
-  wl-clip-persist clipse \
+  wl-clip-persist clipse vlc \
   nautilus sushi gnome-calculator \
-  1password-beta 1password-cli \
-  chromium vlc \
   evince imv
